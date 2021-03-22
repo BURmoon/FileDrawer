67235ce90b971f29f26602245be3e528eae85f1a
来星晨 <68285583+BURmoon@users.noreply.github.com>
2021-03-22 18:58:21 +0800

    Update login_cgi.c

--- 服务端源码_Linux/src_cgi/login_cgi.c
+++ 服务端源码_Linux/src_cgi/login_cgi.c
@@ -55,7 +55,7 @@ int main()
         int len;
         char token[128] = {0};
 
-        printf("Content-type: text/html\r\n\r\n");
+        printf("Content-type: text/plain");
 
         if( contentLength == NULL )
         {
